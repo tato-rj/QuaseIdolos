b761d4b17994063a79b4a3a0f27fe9210b6afd59
Arthur Villar <arthur@iMac.local>
2022-10-26 11:10:37 -0400

    Before translate

--- resources/views/pages/setlist/components/cancel.blade.php
+++ resources/views/pages/setlist/components/cancel.blade.php
@@ -1,12 +1,12 @@
-@modal(['title' => 'Cancelar', 'id' => 'setlist-cancel-'.$entry->id.'-modal'])
+@modal(['title' => 'Cancelar', 'size' => 'sm', 'id' => 'setlist-cancel-'.$entry->id.'-modal'])
 <form method="POST" action="{{route('setlist.cancel', $entry)}}" class="text-center">
 	@csrf
 	@method('DELETE')
 
-	<div class="bg-white text-red rounded text-center mb-4 p-4">
-		@fa(['icon' => 'thumbs-down', 'fa_size' => '5x', 'mr' => 0])
-		<p class="mb-0 mt-3 text-dark fw-bold">Tem certeza que quer cancelar esse pedido?</p>
+	<div class="mx-auto bg-white rounded-circle text-red d-center mb-3" style="width: 80px; height: 80px">
+		@fa(['icon' => 'thumbs-down', 'fa_size' => '2x', 'mr' => 0])
 	</div>
+	<p class="fw-bold">Tem certeza que quer cancelar esse pedido?</p>
 
 	<button class="btn btn-secondary w-100">Sim, pode cancelar</button>
 </form>
--- resources/views/pages/setlist/components/finish.blade.php
+++ resources/views/pages/setlist/components/finish.blade.php
@@ -1,11 +1,11 @@
-@modal(['title' => 'Confirmar', 'id' => 'setlist-complete-'.$entry->id.'-modal'])
+@modal(['title' => 'Confirmar', 'size' => 'sm', 'id' => 'setlist-complete-'.$entry->id.'-modal'])
 <form method="POST" action="{{route('setlist.finish', $entry)}}" class="text-center">
 	@csrf
 
-	<div class="bg-white text-green rounded text-center mb-4 p-4">
-		@fa(['icon' => 'thumbs-up', 'fa_size' => '5x', 'mr' => 0])
-		<p class="text-dark mb-0 mt-3 fw-bold">Confirma que a pessoa cantou a música?</p>
+	<div class="mx-auto bg-white rounded-circle text-green d-center mb-3" style="width: 80px; height: 80px">
+		@fa(['icon' => 'thumbs-up', 'fa_size' => '2x', 'mr' => 0])
 	</div>
+	<p class="fw-bold">Confirma que a pessoa cantou a música?</p>
 
 	<button class="btn btn-secondary w-100">Sim, pode confirmar</button>
 </form>
