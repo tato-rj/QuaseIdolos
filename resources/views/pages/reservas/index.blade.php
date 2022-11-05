9e30d3f1655cb213eeffb8f39818851be74d8a23
Arthur Villar <arthur@iMac.local>
2022-11-04 22:29:35 -0400

    Change font size on lyrics

--- resources/views/pages/reservas/index.blade.php
+++ resources/views/pages/reservas/index.blade.php
@@ -10,22 +10,22 @@
 <section class="container mb-6">
 	<div class="text-center">
 		<h2 class="text-center m-0">QUER AGENTE NO SEU <span class="text-secondary">EVENTO</span>?</h2>
-		<h3 class="mb-5">Solicite a sua reserva aqui!</h3>
+		<h4 class="mb-6">Solicite a sua reserva aqui!</h4>
 
-		<div class="text-center mb-5">
+		<div class="text-center mb-6">
 			<div class="mb-4">
 				@fa(['icon' => 'whatsapp', 'fa_type' => 'b', 'fa_color' => 'secondary', 'fa_size' => '4x'])
 			</div>
-			<h3 class="m-0"><a href="tel:(21) 99115-1374" class="link-none">(21) 99115-1374</a></h3>
+			<h4 class="m-0"><a href="tel:(21) 99115-1374" class="link-none">(21) 99115-1374</a></h4>
 			<h5 class="mb-1">ou</h5>
-			<h3><a href="tel:(21) 99115-1374" class="link-none">(21) 99274-9006</a></h3>
+			<h4><a href="tel:(21) 99115-1374" class="link-none">(21) 99274-9006</a></h4>
 		</div>
 
 		<div class="text-center">
 			<div class="mb-4">
 				@fa(['icon' => 'envelope', 'fa_color' => 'secondary', 'fa_size' => '4x'])
 			</div>
-			<h3><a href="mailto:quaseidolos@gmail.com" class="link-none">quaseidolos@gmail.com</a></h3>
+			<h4><a href="mailto:quaseidolos@gmail.com" class="link-none">quaseidolos@gmail.com</a></h4>
 		</div>
 
 	</div>
