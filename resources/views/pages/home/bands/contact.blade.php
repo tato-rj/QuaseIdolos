85921f1708a6411eaadb78e18376229ae5904f10
Arthur Villar <arthur@iMac.local>
2022-11-06 01:12:01 -0400

    Fix draggable bug

--- resources/views/pages/home/bands/contact.blade.php
+++ resources/views/pages/home/bands/contact.blade.php
@@ -1,20 +1,22 @@
 <section class="container mb-8">
 	<div class="row">
 		<div class="col-lg-8 col-md-10 col-12 mx-auto">
-			<div class="row">
-				<div class="col-lg-4 col-10 mx-auto">
-					<div class="position-relative" style="transform: rotate(5deg);">
-						<div class="bg-secondary w-100 h-100 position-absolute left-0 top-0" style="transform: rotate(5deg);"></div>
-						<video id="player" data-poster="{{asset('images/video-poster.jpeg')}}">
-						  <source src="{{asset('videos/promo.mp4')}}" type="video/mp4" />
-						</video>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-4 col-10 mx-auto">
+						<div class="position-relative" style="transform: rotate(5deg);">
+							<div class="bg-secondary w-100 h-100 position-absolute left-0 top-0" style="transform: rotate(5deg);"></div>
+							<video id="player" data-poster="{{asset('images/video-poster.jpeg')}}">
+							  <source src="{{asset('videos/promo.mp4')}}" type="video/mp4" />
+							</video>
+						</div>
 					</div>
-				</div>
-				<div class="col-lg-8 col-10 mx-auto d-center py-3">
-					<div>
-						<h3>CANTAR COM UMA BANDA É UMA <span class="text-secondary">EXPERIÊNCIA SEM IGUAL</span>.</h3>
-						<h6 class="mb-4">Entre em contato com a gente e contrate a Quase ídolos pra tornar o seu evento inesquecível!</h6>
-						<a href="{{route('reservas')}}" class="btn btn-secondary btn-lg">FAÇA A SUA RESERVA</a>
+					<div class="col-lg-8 col-10 mx-auto d-center py-3">
+						<div>
+							<h3>CANTAR COM UMA BANDA É UMA <span class="text-secondary">EXPERIÊNCIA SEM IGUAL</span>.</h3>
+							<h6 class="mb-4">Entre em contato com a gente e contrate a Quase ídolos pra tornar o seu evento inesquecível!</h6>
+							<a href="{{route('reservas')}}" class="btn btn-secondary btn-lg">FAÇA A SUA RESERVA</a>
+						</div>
 					</div>
 				</div>
 			</div>
