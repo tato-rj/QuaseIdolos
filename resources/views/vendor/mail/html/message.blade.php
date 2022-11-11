069a7511327b3efb630e8a4519fe2d9f7c8b0180
Arthur Villar <arthur@iMac.local>
2022-11-11 14:48:16 -0500

    Implement mail

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -23,7 +23,7 @@
 {{-- Footer --}}
 @slot('footer')
 @component('mail::footer')
-<div class="d-center mb-2">
+<div class="d-center mb-2" style="opacity: .4">
     <a href="{{config('social.facebook')}}" target="_blank" class="mx-2"><img src="{{asset('images/icons/facebook.svg')}}" alt="" width="32"></a>
     <a href="{{config('social.instagram')}}" target="_blank" class="mx-2"><img src="{{asset('images/icons/instagram.svg')}}" alt="" width="32"></a>
 </div>
