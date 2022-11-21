4ad8b293e8b0fb97c0b0f452ed50f3e45a8c6e53
Arthur Villar <arthur@iMac.local>
2022-11-21 14:05:14 -0500

    Improve backend

--- app/Http/Controllers/SongsController.php
+++ app/Http/Controllers/SongsController.php
@@ -14,7 +14,7 @@ class SongsController extends Controller
      */
     public function index()
     {
-        $songs = Song::orderBy('name')->paginate(8);
+        $songs = Song::orderBy('name')->paginate(24);
 
         return view('pages.songs.index', compact('songs'));
     }
