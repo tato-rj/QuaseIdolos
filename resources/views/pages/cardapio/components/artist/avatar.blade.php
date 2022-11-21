a3639d9139a7d6dccda66acd2a96bb35b13a17c5
Arthur Villar <arthur@iMac.local>
2022-11-21 12:04:33 -0500

    Add columns to lyrics

--- resources/views/pages/cardapio/components/artist/avatar.blade.php
+++ resources/views/pages/cardapio/components/artist/avatar.blade.php
@@ -1,9 +1,11 @@
-<div class="artist m-3 {{iftrue($selected ?? null, 'mb-5')}}" {{iftrue($selected ?? null, 'selected')}}>
+<div class="artist m-3 mb-5 {{iftrue($selected ?? null, 'mb-5')}}" {{iftrue($selected ?? null, 'selected')}}>
 	<div class="d-center flex-column">
 		<div class="position-relative">
 			<img src="{{$artist->coverImage()}}" class="rounded-circle mb-2">
 			<span class="bg-secondary text-dark rounded-circle position-absolute d-center font-cursive">{{$artist->songs_count}}</span>
 		</div>
-		<h6 class="w-100 text-center m-0 text-truncate" style="font-size: {{iftrue($selected ?? null, '1.4rem')}}">{{$artist->name}}</h6>
+		<div class="w-100">
+			<h6 class="text-center m-0 text-truncate lh-1" style="font-size: {{iftrue($selected ?? null, '1.4rem')}}">{{$artist->name}}</h6>
+		</div>
 	</div>
 </div>
