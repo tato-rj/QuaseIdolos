819558ecb0b1dd9a91b26393a2c69c09409d7b0b
Arthur Villar <arthur@iMac.local>
2022-11-21 12:12:55 -0500

    Add columns to lyrics

--- resources/views/pages/cardapio/results/table.blade.php
+++ resources/views/pages/cardapio/results/table.blade.php
@@ -1,6 +1,8 @@
 <div class="mt-">
 	@unless($songs->isEmpty())
-	<h6 id="clear-results" class="text-center mb-3 cursor-pointer">@fa(['icon' => 'eraser'])limpar pesquisa</h6>
+	<div class="text-center">
+		<button id="clear-results" class="btn btn-secondary mb-3">@fa(['icon' => 'eraser'])limpar pesquisa</button>
+	</div>
 	@endunless
 	@foreach($songs as $song)
 	@include('pages.cardapio.results.row')
