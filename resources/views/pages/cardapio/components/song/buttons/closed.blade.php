16871c6ec90565a64bd9e0ad1a809855780ed718
Arthur Villar <arthur@iMac.local>
2022-11-26 11:02:05 -0500

    Fix gig join on cardapio page

--- resources/views/pages/cardapio/components/song/buttons/closed.blade.php
+++ resources/views/pages/cardapio/components/song/buttons/closed.blade.php
@@ -1,2 +1,2 @@
 {{-- <p class="text-center">@fa(['icon' => 'info-circle'])Não estamos recebendo pedidos agora, veja <a class="link-secondary" href="{{route('calendar.index')}}">aqui</a> o nosso calendário de shows.</p> --}}
-<p class="text-center">@fa(['icon' => 'info-circle'])Não estamos recebendo pedidos agora, aguarde o show começar para pedir a sua música.</p>
+<p class="text-center">@fa(['icon' => 'info-circle'])Não estamos recebendo pedidos agora.</p>
