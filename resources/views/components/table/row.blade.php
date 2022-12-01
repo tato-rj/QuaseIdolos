fd3a7f898f582ee5fb21548ef2a2ffb247af46f0
Arthur Villar <arthur@iMac.local>
2022-12-01 12:26:07 -0500

    Redo all tables

--- resources/views/components/table/row.blade.php
+++ resources/views/components/table/row.blade.php
@@ -1,11 +1,11 @@
 <tr class="text-white table-row">
 	@for($i=1;$i<=5;$i++)
 	@isset(${'column'.$i})
-	<td class="align-middle p-3"><h6 class="m-0">{!!${'column'.$i}!!}</h6></td>
+	<td class="align-middle p-3 h6">{!!${'column'.$i}!!}</td>
 	@endisset
 	@endfor
 
 	@isset($actions)
-	<td class="text-right align-middle p-3"><h6 class="m-0">{!!$actions!!}</h6></td>
+	<td class="text-right align-middle p-3 h6">{!!$actions!!}</td>
 	@endisset
 </tr>
