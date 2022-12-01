b9106942c1ede04d36406beb0898678bdb43e355
Arthur Villar <arthur@iMac.local>
2022-12-01 13:21:07 -0500

    Redo all tables

--- resources/views/pages/cardapio/results/row.blade.php
+++ resources/views/pages/cardapio/results/row.blade.php
@@ -3,7 +3,7 @@
 @row
   @slot('column1')
   <div class="d-flex">
-		<img src="{{$song->artist->coverImage()}}" class="rounded-circle mr-2" style="width: 43px;">
+		<img src="{{$song->artist->coverImage()}}" class="rounded-circle mr-2" style="width: 43px; height: 43px">
 		<div>
 			<a href="#" data-bs-toggle="modal" data-bs-target="#song-{{$song->id}}-modal" class="link-none mr-2">{{$song->name}}</a>
 			@include('components.song.user.icons')
