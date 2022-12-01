2e311da21c909013d09a85b9e42ee4f1e3319843
Arthur Villar <arthur@iMac.local>
2022-12-01 12:29:17 -0500

    Redo all tables

--- resources/views/pages/cardapio/components/song/header.blade.php
+++ resources/views/pages/cardapio/components/song/header.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex align-items-center">
+<div class="d-flex align-items-center text-left">
 	<img src="{{$song->artist->coverImage()}}" style="width: 76px; height: 76px; border: 6px solid white;" class="rounded-circle mr-2">
 	<div>
 		<h3 class="mb-0 font-cursive">{{$song->name}}</h3>
