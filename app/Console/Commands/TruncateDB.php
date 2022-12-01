2c9cbc1a3abaec013dadb49b85009fddeaecd81a
Arthur Villar <arthur@iMac.local>
2022-12-01 12:31:59 -0500

    Redo all tables

--- app/Console/Commands/TruncateDB.php
+++ app/Console/Commands/TruncateDB.php
@@ -12,7 +12,7 @@ class TruncateDB extends Command
      * @var string
      */
     protected $signature = 'truncate:table {table}';
-    protected $devTables = ['gigs', 'song_requests', 'ratings', 'participants', 'favorites'];
+    protected $devTables = ['venues', 'gigs', 'song_requests', 'ratings', 'participants', 'favorites'];
 
     /**
      * The console command description.
