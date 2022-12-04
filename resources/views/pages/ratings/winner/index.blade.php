0c946b87e749cbc8b01b4ec2df1c381dfc97e782
Arthur Villar <arthur@iMac.local>
2022-12-04 12:04:48 -0500

    Fix stats page

--- resources/views/pages/ratings/winner/index.blade.php
+++ resources/views/pages/ratings/winner/index.blade.php
@@ -21,16 +21,6 @@ canvas {
 @section('content')
 <section class="container pt-5">
 	@pagetitle(['title' => 'Resultado ', 'highlight' => 'final'])
-   <div class="d-center mb-2">
-      <div class="text-center mx-3">
-         <h3 class="mb-0">@fa(['icon' => 'users', 'classes' => 'opacity-4 no-stroke']){{$ranking->votersCount}}</h3>
-         <h5 class="text-secondary">Pessoas votaram</h5>
-      </div>
-      <div class="text-center mx-3">
-         <h3 class="mb-0">@fa(['icon' => 'music', 'classes' => 'opacity-4 no-stroke']){{$ranking->ratings->count()}}</h3>
-         <h5 class="text-secondary">Músicas votadas</h5>
-      </div>
-   </div>
 </section>
 
 <section class="container" id="winner-container">
