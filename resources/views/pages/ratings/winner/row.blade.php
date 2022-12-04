2941b142796e405f1042ad34e4970980bcfebfc3
Arthur Villar <arthur@iMac.local>
2022-12-04 12:09:04 -0500

    Fix stats page

--- resources/views/pages/ratings/winner/row.blade.php
+++ resources/views/pages/ratings/winner/row.blade.php
@@ -5,15 +5,16 @@
                <div class="d-flex align-items-center mr-1">
                   <div class="mr-2" style="width: 78px">
                      @if($user->hasAvatar())
-                     @include('components.avatar.image', ['size' => '68px'])
+                     @include('components.avatar.image', ['size' => '78px'])
                      @else
-                     @include('components.avatar.initial', ['size' => '68px', 'fontsize' => '1.8rem'])
+                     @include('components.avatar.initial', ['size' => '78px', 'fontsize' => '1.8rem'])
                      @endif
                   </div>
                   <div class="mr-2">
                      <div class="d-flex flex-column">
                         <h3 class="mb-0 text-dark no-stroke">{{$user->name}}</h3>
                         <h3 class="mb-0 text-primary no-stroke">{{$song->name}}</h3>
+                        <h6 class="mb-0 text-dark no-stroke opacity-8">{{$song->artist->name}}</h6>
                      </div>
                   </div>
                </div>
