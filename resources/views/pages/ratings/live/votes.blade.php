36aadb915e76a3eccb8e88996fa93709bfa6b3ba
Arthur Villar <arthur@iMac.local>
2022-12-04 12:03:28 -0500

    Fix stats page

--- resources/views/pages/ratings/live/votes.blade.php
+++ resources/views/pages/ratings/live/votes.blade.php
@@ -7,6 +7,10 @@
 		<h3 class="mb-0">@fa(['icon' => 'music', 'classes' => 'opacity-4 no-stroke']){{$ratings->groupBy('song_request_id')->count()}}</h3>
 		<h5 class="text-secondary">Músicas votadas</h5>
 	</div>
+	<div class="text-center mx-3">
+		<h3 class="mb-0">@fa(['icon' => 'vote-yea', 'classes' => 'opacity-4 no-stroke']){{$ratings->count()}}</h3>
+		<h5 class="text-secondary">Total de votos</h5>
+	</div>
 </div>
 
 <div class="text-center" style="display: {{$ratings->count() ? 'block' : 'none'}}">
