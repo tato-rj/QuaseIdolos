ed09b67a3dbe2d1064fcb4f957aa2631bffc85b0
Arthur Villar <arthur@iMac.local>
2022-12-16 01:12:40 -0500

    Auto adjust lyrics

--- resources/views/pages/songs/lyrics/index.blade.php
+++ resources/views/pages/songs/lyrics/index.blade.php
@@ -66,7 +66,7 @@ $.fn.isInViewport = function() {
 var elementTop = $(this).offset().top;
 var elementBottom = elementTop + $(this).outerHeight();
 var viewportBottom = $(window).height();
-return elementBottom < viewportBottom - 50;
+return elementBottom < viewportBottom - 100;
 };
 
 function adjustFontsize(id)
@@ -88,7 +88,7 @@ function adjustFontsize(id)
 }
 
 $(document).ready(function() {
-	// adjustFontsize('#lyrics');
+	adjustFontsize('#lyrics');
 });
 
 var resizeTimer;
