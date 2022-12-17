7e6da385931459fed8737861e0a381c1c36512b7
Arthur Villar <arthur@iMac.local>
2022-12-17 01:34:17 -0500

    Fix song request bug

--- resources/views/pages/ratings/winner/stats.blade.php
+++ resources/views/pages/ratings/winner/stats.blade.php
@@ -4,11 +4,11 @@
       <h5 class="text-secondary">Pessoas votaram</h5>
    </div>
    <div class="text-center mx-3">
-      <h3 class="mb-0">@fa(['icon' => 'music', 'classes' => 'opacity-4 no-stroke']){{$ratings->groupBy('song_request_id')->count()}}</h3>
+      <h3 class="mb-0">@fa(['icon' => 'music', 'classes' => 'opacity-4 no-stroke']){{$ratings->count()}}</h3>
       <h5 class="text-secondary">Músicas votadas</h5>
    </div>
    <div class="text-center mx-3">
-      <h3 class="mb-0">@fa(['icon' => 'vote-yea', 'classes' => 'opacity-4 no-stroke']){{$ratings->count()}}</h3>
+      <h3 class="mb-0">@fa(['icon' => 'vote-yea', 'classes' => 'opacity-4 no-stroke']){{$ratings->flatten()->count()}}</h3>
       <h5 class="text-secondary">Total de votos</h5>
    </div>
 </div>
