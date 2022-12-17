c8a1ce58f5d0e9d6e430aee37cf079908a72d332
Arthur Villar <arthur@iMac.local>
2022-12-17 14:07:52 -0500

    Add scroll top button

--- resources/views/components/scrolltop.blade.php
+++ resources/views/components/scrolltop.blade.php
@@ -1,5 +1,7 @@
 <div id="scroll-top" class="position-fixed" style="bottom: 20px; right: 20px; display: none;">
-<button class="btn-raw bg-secondary rounded-circle d-center opacity-8" style="width: 80px; height: 80px;">
-	@fa(['icon' => 'chevron-up', 'mr' => 0, 'fa_size' => '2x'])
-</button>
+	<div class="d-block d-sm-none">
+	<button class="btn-raw bg-secondary rounded-circle d-center opacity-8" style="width: 80px; height: 80px;">
+		@fa(['icon' => 'chevron-up', 'mr' => 0, 'fa_size' => '2x'])
+	</button>
+	</div>
 </div>
