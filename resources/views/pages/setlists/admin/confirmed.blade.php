5c4bc5ab51364f27bd9d29d47274c7956740c64f
Arthur Villar <arthur@iMac.local>
2022-12-17 14:29:45 -0500

    Add scroll top button

--- resources/views/pages/setlists/admin/confirmed.blade.php
+++ resources/views/pages/setlists/admin/confirmed.blade.php
@@ -1,11 +1,11 @@
 <div class="confirmed-request mx-auto mb-3">
 	<div class="rounded border-secondary event-box py-3 px-2 row">
 		<div class="col-12 d-flex justify-content-between mb-3">
-			<div class="">
-				<h2 class="no-stroke text-primary font-cursive">{{$entry->user->firstName}}</h2>
+			<div class="text-truncate">
+				<h2 class="no-stroke text-primary font-cursive text-truncate">{{$entry->user->firstName}}</h2>
 				<div class="d-flex align-items-center">
 					<img src="{{$entry->song->artist->coverImage()}}" class="rounded-circle mr-3" style="width: 56px">
-					<div>
+					<div class="text-truncate">
 						<h4 class="text-dark no-stroke m-0 text-truncate">{{$entry->song->name}}</h4>
 						<p class="text-dark no-stroke m-0 text-truncate fw-bold">{{$entry->song->artist->name}}</p>
 					</div>
