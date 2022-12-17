4d97567cdc30bf62447d80137b478a1f09b55ddf
Arthur Villar <arthur@iMac.local>
2022-12-17 00:29:26 -0500

    Fix genre card zindex

--- resources/views/pages/genres/card.blade.php
+++ resources/views/pages/genres/card.blade.php
@@ -1,6 +1,7 @@
 <a href="{{route('cardapio.index', ['estilo' => $genre->slug])}}" class="link-none">
 	<div class="rounded py-2 bg-center position-relative d-center m-2" style="background-image: url({{$genre->coverImage()}}); width: 160px; height: 75px;">
-		<div class="bg-black opacity-4 position-absolute w-100 h-100 top-0 left-0 rounded" style="z-index: 1"></div>
-		<h4 class="m-0 text-truncate" style="z-index: 2">{{$genre->name}}</h4>
+		<h4 class="m-0 text-truncate">{{$genre->name}}</h4>
+		<div class="bg-black opacity-4 position-absolute w-100 h-100 top-0 left-0 rounded"></div>
+		
 	</div>
 </a>
