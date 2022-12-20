0464b381fca248ae60b7b0f2cd6c13640e4a22df
Arthur Villar <arthur@iMac.local>
2022-12-20 01:07:24 -0500

    Improve gig status feedback

--- resources/views/pages/setlists/user/banner/songs.blade.php
+++ resources/views/pages/setlists/user/banner/songs.blade.php
@@ -1,11 +1,11 @@
 <div class="row" id="toggle-waiting-list" data-target="#waiting-list-banner">
 	<div class="col-lg-6 col-md-10 col-12 mx-auto py-2">
-		<div class="d-apart px-2">
+		<div class="d-apart px-3">
 			<div class="text-truncate">
 				<h6 class="text-primary m-0 no-stroke">{{$songRequests->count()}} Pedidos na lista</h6>
 			</div>
 
-			<button class="btn-raw text-primary no-stroke px-2">@fa(['icon' => 'chevron-up', 'mr' => 0])</button>
+			<button class="btn-raw text-primary no-stroke">@fa(['icon' => 'chevron-up', 'mr' => 0])</button>
 		</div>
 	</div>
 </div>
