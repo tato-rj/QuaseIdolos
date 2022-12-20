87bbc28c0f9d4174d48bcfd15f20ee4fd723bb19
Arthur Villar <arthur@iMac.local>
2022-12-20 01:24:01 -0500

    Improve gig status feedback

--- resources/views/pages/cardapio/components/song/changeRequest.blade.php
+++ resources/views/pages/cardapio/components/song/changeRequest.blade.php
@@ -1,14 +1,15 @@
 <div style="display: none" id="song-requests-modal-{{$song->id}}">
 	<h5 class="text-center mb-4">Trocar essa música por...</h5>
 
-	@foreach($songRequests as $songRequest)
-	@admin
-	@include('pages.cardapio.components.songRequest.admin')
-	@else
-	@include('pages.cardapio.components.songRequest.user')
-	@endadmin
-	@endforeach
-
+	<div class="mb-4">
+		@foreach($songRequests as $songRequest)
+		@admin
+		@include('pages.cardapio.components.songRequest.admin')
+		@else
+		@include('pages.cardapio.components.songRequest.user')
+		@endadmin
+		@endforeach
+	</div>
 	
 	<div class="text-right">
 		<button name="change-song" 
