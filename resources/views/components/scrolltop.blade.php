4172f8aea398bfdf2ce86824713c582bcedea5ef
Arthur Villar <arthur@iMac.local>
2022-12-20 01:03:44 -0500

    Improve gig status feedback

--- resources/views/components/scrolltop.blade.php
+++ resources/views/components/scrolltop.blade.php
@@ -1,7 +1,5 @@
 <div id="scroll-top" class="position-fixed x-auto" style="top: 20px; display: none;">
-	<div class="d-block d-sm-none">
 	<button class="btn btn-sm btn-secondary opacity-8 text-truncate">
 		@fa(['icon' => 'chevron-up'])voltar ao topo
 	</button>
-	</div>
 </div>
