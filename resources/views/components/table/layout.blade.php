8d29dc30483574dfdb131bd4a409f75c38de50ca
Arthur Villar <arthur@iMac.local>
2022-12-21 11:01:39 -0500

    Improve gig status

--- resources/views/components/table/layout.blade.php
+++ resources/views/components/table/layout.blade.php
@@ -8,12 +8,6 @@
   @isset($title)
   <h4 class="">@fa(['icon' => 'list-ul']){{$title}}</h4> 
   @endisset
-
-  @isset($legend)
-  <div>
-    <label class="mb-3">Total de {{$total}} @choice($legend, $total)</label>
-  </div>
-  @endisset
   
   @include('components.empty')
   @endisset
