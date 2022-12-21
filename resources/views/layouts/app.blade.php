e75e200f64d53cbdff9adfb7cf3db7201f1a4962
Arthur Villar <arthur@iMac.local>
2022-12-21 13:25:08 -0500

    Improve gig status

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -433,7 +433,7 @@ $(document).ready(function() {
                 let $element = $(this);
                 $element.datepicker({
                     minDate: new Date(),
-                    // defaultDate: new Date($element.data('datepicker')),
+                    defaultDate: new Date($element.data('datepicker')),
                     onSelect: function() {
                         if ($element.attr('data-input'))
                             $($element.data('input')).val(this.value);
