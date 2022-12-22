d782e6fe236e898d82bfc2c198d24cda4345d57f
Arthur Villar <arthur@iMac.local>
2022-12-21 22:10:38 -0500

    Quick update

--- resources/views/pages/setlists/admin/counter.blade.php
+++ resources/views/pages/setlists/admin/counter.blade.php
@@ -2,7 +2,13 @@
 
 	<div class="mt-3 w-100 rounded-pill setlist-counter mx-auto mb-4" style="height: 40px; background: rgba(0,0,0,0.1)">
 		<div class="font-cursive setlist-counter-fill rounded-pill d-flex align-items-center justify-content-end px-3" 
-			style="width: {{$percentage}}%;">{{$setlist->count()}}</div>
+			style="width: {{$percentage}}%;">
+			@if($setlist->isFull())
+			<span class="w-100 text-center">Setlist completo!</span>
+			@else
+			{{$setlist->count()}}
+			@endif
+		</div>
 	</div>
 
 @endif
