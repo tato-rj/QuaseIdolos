dca4979b1fa1fd5a575bd5f1dee4c612aa0926e9
Arthur Villar <arthur@iMac.local>
2022-12-23 11:04:57 -0500

    Quick update

--- resources/views/pages/gigs/modals/delete.blade.php
+++ resources/views/pages/gigs/modals/delete.blade.php
@@ -1,9 +1,6 @@
 @modal(['title' => 'Tem certeza?','id' => 'delete-gig-'.$gig->id.'-modal'])
 @form(['method' => 'DELETE', 'url' => route('gig.destroy', $gig), 'data' => ['trigger' => 'loader']])
 
-	@csrf
-	@method('DELETE')
-
 	<div class="text-left bg-white px-4 py-3 rounded mb-3">
 		<p class="text-danger mb-1"><strong>@fa(['icon' => 'exclamation-circle'])Essa ação é irreversível</strong></p>
 		<p class="text-dark mb-1">O setlist desse evento <u>não</u> será removido, esses registros vão ficar sempre disponíveis para os usuários.</p>
