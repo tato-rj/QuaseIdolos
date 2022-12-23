45ed3b59ec872988e813b158b98d0845a558c312
Arthur Villar <arthur@iMac.local>
2022-12-22 19:30:43 -0500

    Quick update

--- resources/views/pages/gigs/modals/password.blade.php
+++ resources/views/pages/gigs/modals/password.blade.php
@@ -2,7 +2,7 @@
 
 @modal(['title' => $gig->name(), 'id' => 'select-gig-modal', 'autoshow' => true])
 
-<h6>Começamos o evento no {{$gig->name()}}, se tiver a senha pra participar é só escrever abaixo!</h6>
+<h6 class="mb-3">Começamos o evento no {{$gig->name()}}, se tiver a senha pra participar é só escrever abaixo!</h6>
 @include('pages.gigs.join.card', ['showPassword' => true])
 
 @endmodal
