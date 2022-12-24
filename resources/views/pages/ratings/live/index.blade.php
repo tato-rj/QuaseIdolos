c34b073b9239cca2722ac92ce5857a2d25e5c271
Arthur Villar <arthur@iMac.local>
2022-12-23 19:30:24 -0500

    Improve share screens

--- resources/views/pages/ratings/live/index.blade.php
+++ resources/views/pages/ratings/live/index.blade.php
@@ -9,6 +9,18 @@
 @section('content')
 <section class="container-fluid mb-6 pt-5 position-relative">
 	@pagetitle(['title' => 'Votação ', 'highlight' => 'ao vivo'])
+   
+   <button data-bs-toggle="modal" data-bs-target="#confirm-winner-modal" class="btn btn-secondary mx-auto mb-4">Ver ganhador</button>
+
+   @modal(['title' => 'Tem certeza?','id' => 'confirm-winner-modal'])
+      <div class="text-left bg-white px-4 py-3 rounded mb-3">
+         <p class="text-danger mb-1"><strong>@fa(['icon' => 'exclamation-circle'])Atenção</strong></p>
+         <p class="text-dark mb-1">Ao continuar a votação desse evento será encerrada.</p>
+         <p class="text-dark m-0">Quer continuar?</p>
+      </div>
+      <a href="{{route('ratings.winner')}}" class="btn btn-secondary">Sim, ver ganhador</a>
+   @endmodal
+
 	<div id="ranking-container">
 
 	</div>
