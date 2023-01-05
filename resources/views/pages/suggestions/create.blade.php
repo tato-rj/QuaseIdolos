9992bc5bca9891df582d42cf96b632916e8459f8
Arthur Villar <arthur@iMac.local>
2023-01-05 12:29:58 -0500

    Create suggestion option to users

--- resources/views/pages/suggestions/create.blade.php
+++ resources/views/pages/suggestions/create.blade.php
@@ -1,7 +1,7 @@
-@modal(['title' => 'Pedir música','id' => 'suggestion-modal'])
+@modal(['title' => 'Pedir uma música','id' => 'suggestion-modal'])
 @form(['method' => 'POST', 'url' => route('suggestions.store'), 'data' => ['trigger' => 'loader']])
 	@input(['placeholder' => 'Nome do artista', 'required' => true, 'name' => 'artist_name', 'value' => old('artist_name')])
 	@input(['placeholder' => 'Nome da música', 'required' => true, 'name' => 'song_name', 'value' => old('song_name')])
-	@submit(['label' => 'Enviar minha sugestão', 'theme' => 'secondary'])
+	@submit(['label' => 'Enviar a minha sugestão', 'theme' => 'secondary'])
 @endform
 @endmodal
