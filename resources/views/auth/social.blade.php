3f651be2d7c9611a52680e57242e652caaa189eb
Arthur Villar <arthur@iMac.local>
2023-01-09 15:45:29 -0500

    Quick update

--- resources/views/auth/social.blade.php
+++ resources/views/auth/social.blade.php
@@ -1,6 +1,6 @@
 <div class="mb-4">
 	<h6 class="text-center mb-3">OU</h6>
-	@include('components.core.forms.social', ['service' => 'github'])
+	{{-- @include('components.core.forms.social', ['service' => 'github']) --}}
 	{{-- @include('components.core.forms.social', ['service' => 'instagram']) --}}
 	@include('components.core.forms.social', ['service' => 'facebook'])
 	@include('components.core.forms.social', ['service' => 'google'])
