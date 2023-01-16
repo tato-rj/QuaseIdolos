8effbcdca080192a04c01501c2816812095ed486
Arthur Villar <arthur@iMac.local>
2023-01-16 15:42:56 -0500

    Quick update

--- resources/views/layouts/menu/guest/links.blade.php
+++ resources/views/layouts/menu/guest/links.blade.php
@@ -2,4 +2,4 @@
 {{-- <a class="nav-link bg-secondary rounded-pill px-4 py-1 mb-3" href="{{route('about')}}">A Banda</a> --}}
 <a class="nav-link bg-secondary rounded-pill px-4 py-1 mb-3" href="{{route('cardapio.index')}}">Cardápio</a>
 <a class="nav-link bg-secondary rounded-pill px-4 py-1 mb-3" href="{{route('reservas')}}">Reservas</a>
-<a class="nav-link bg-secondary rounded-pill px-4 py-1 mb-3" href="{{route('calendar.index')}}">Calendário</a>
+{{-- <a class="nav-link bg-secondary rounded-pill px-4 py-1 mb-3" href="{{route('calendar.index')}}">Calendário</a> --}}
