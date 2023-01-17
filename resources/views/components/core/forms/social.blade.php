9a36931d85ff0b45d4afa71e9a6e188bbf0af67f
Arthur Villar <arthur@iMac.local>
2023-01-17 12:28:09 -0500

    Quick update

--- resources/views/components/core/forms/social.blade.php
+++ resources/views/components/core/forms/social.blade.php
@@ -1,1 +1,1 @@
-<a href="{{route('socialite', $service)}}" class="btn btn-outline-secondary w-100 mb-2">@fa(['icon' => $service, 'fa_type' => 'b'])ENTRAR COM {{strtoupper($service)}}</a>
+<a href="{{route('socialite', $service)}}" class="btn btn-{{$service}} w-100 mb-2">@fa(['icon' => $service, 'fa_type' => 'b'])ENTRAR COM {{strtoupper($service)}}</a>
