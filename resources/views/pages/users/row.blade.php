5cc7849e59025f525a2b16e8510f984d8f926846
Arthur Villar <arthur@iMac.local>
2023-01-17 13:29:08 -0500

    Quick update

--- resources/views/pages/users/row.blade.php
+++ resources/views/pages/users/row.blade.php
@@ -5,7 +5,7 @@
 		<a href="{{route('users.edit', $user)}}" class="link-secondary">
       <span class="mr-1 align-middle">{{$user->name}}</span>
       @foreach($user->socialAccounts as $socialAccount)
-      @fa(['fa_type' => 'b', 'icon' => $socialAccount->social_provider, 'mr' => 1, 'classes' => 'align-middle'])
+      @fa(['fa_type' => 'b', 'icon' => $socialAccount->social_provider, 'mr' => 1, 'classes' => 'align-middle', 'fa_color' => 'white'])
       @endforeach
     </a>
   @endslot
