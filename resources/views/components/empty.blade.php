5ff6093eaa657363c8f6b6ffeaed6866cb49031a
Arthur Villar <arthur@iMac.local>
2023-01-18 12:02:13 -0500

    Quick update

--- resources/views/components/empty.blade.php
+++ resources/views/components/empty.blade.php
@@ -1,3 +1,3 @@
-<div class="col-12 pt-{{$pt ?? 5}} text-center opacity-4 empty-content">
+<div class="col-12 py-4 text-center opacity-4 empty-content">
 	@fa(['icon' => 'box-open', 'mr' => 0, 'fa_size' => '6x', 'classes' => 'mb-2'])
 </div>
