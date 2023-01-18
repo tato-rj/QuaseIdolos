789543046222611a0400e29431da8f6bc3c98ad3
Arthur Villar <arthur@iMac.local>
2023-01-18 12:56:37 -0500

    Quick update

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,9 +1,9 @@
 <div class="d-center flex-wrap">
   @foreach($pages as $title => $route)
   @if(request()->url() == $route)
-  <button class="btn btn-outline-secondary {{! $loop->last ? 'mr-2' : null}}" disabled>{{$title}}</button>
+  <button class="btn btn-outline-secondary mb-2 {{! $loop->last ? 'mr-2' : null}}" disabled>{{$title}}</button>
   @else
-  <a href="{{$route}}" class="btn btn-secondary {{! $loop->last ? 'mr-2' : null}}">{{$title}}</a>
+  <a href="{{$route}}" class="btn btn-secondary mb-2 {{! $loop->last ? 'mr-2' : null}}">{{$title}}</a>
   @endif
   @endforeach
 </div>
