4e845f1a96fcd893c7887cb2e97f76f9107a495c
Arthur Villar <arthur@iMac.local>
2023-01-18 11:05:50 -0500

    Quick update

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -33,7 +33,7 @@ class CreateNewUser implements CreatesNewUsers
         ])->validate();
 
         return User::create([
-            'name' => $input['name'],
+            'name' => ucwords(strtolower($input['name'])),
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
