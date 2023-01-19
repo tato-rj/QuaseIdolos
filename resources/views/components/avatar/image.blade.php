d808cfebe2575386a6cd166793a3e63695b3b341
Arthur Villar <arthur@iMac.local>
2023-01-18 23:13:24 -0500

    Quick update

--- resources/views/components/avatar/image.blade.php
+++ resources/views/components/avatar/image.blade.php
@@ -1,14 +1,14 @@
 <div 
 @isset($offcanvas)
-class="nav-link d-center cursor-pointer position-relative"
+class="nav-link d-center cursor-pointer position-relative no-truncate"
 href="#" 
 data-bs-toggle="offcanvas" 
 data-bs-target="#offcanvasUserMenu"
 @else
-class="nav-link d-center position-relative"
+class="nav-link d-center position-relative no-truncate"
 @endisset
 >
-  <img src="{{$user->avatar()}}" class="rounded-circle w-100 no-truncate" style="max-width: {{$size ?? '39.2px'}}">
+  <img src="{{$user->avatar()}}" class="rounded-circle w-100" style="max-width: {{$size ?? '39.2px'}}">
 
   @if(isset($star) && $user->isAdmin())
   <span class="position-absolute bottom-0 right-0">@fa(['icon' => 'star', 'fa_color' => 'yellow', 'fa_size' => 'lg'])</span>
--- resources/views/components/avatar/initial.blade.php
+++ resources/views/components/avatar/initial.blade.php
@@ -1,14 +1,14 @@
 <div style="width: {{$size ?? '39.2px'}}; height: {{$size ?? '39.2px'}};"
 @isset($offcanvas)
-class="nav-link bg-secondary rounded-circle d-center cursor-pointer position-relative"
+class="nav-link bg-secondary rounded-circle d-center cursor-pointer position-relative no-truncate"
 href="#" 
 data-bs-toggle="offcanvas" 
 data-bs-target="#offcanvasUserMenu"
 @else
-class="nav-link bg-secondary rounded-circle d-center mx-auto position-relative"
+class="nav-link bg-secondary rounded-circle d-center mx-auto position-relative no-truncate"
 @endisset
 >
-  <span class="font-cursive no-truncate" style="font-size: {{percent(40)->of($size ?? '39.2')}}px;">{{$user->initial}}</span>
+  <span class="font-cursive" style="font-size: {{percent(40)->of($size ?? '39.2')}}px;">{{$user->initial}}</span>
 
   @if(isset($star) && $user->isAdmin())
   <span class="position-absolute bottom-0 right-0">@fa(['icon' => 'star', 'fa_color' => 'yellow', 'fa_size' => 'lg'])</span>
