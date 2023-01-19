34572f9eddb478dc7f77b81ece69928586648e38
Arthur Villar <arthur@iMac.local>
2023-01-19 10:25:13 -0500

    Quick update

--- resources/views/layouts/components/favicon.blade.php
+++ resources/views/layouts/components/favicon.blade.php
@@ -15,3 +15,9 @@
 <meta name="msapplication-TileColor" content="#ffffff">
 <meta name="msapplication-TileImage" content="{{asset('favicon/ms-icon-144x144.png')}}">
 <meta name="theme-color" content="#ffffff">
+<meta name="mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black">
+<meta name="apple-mobile-web-app-title" content="WebApp">
