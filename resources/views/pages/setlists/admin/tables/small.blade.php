06a17324bdd12d12eaea122a35c735eccad0b0f0
Arthur Villar <arthur@iMac.local>
2023-01-23 16:31:04 -0500

    Quick update

--- resources/views/pages/setlists/admin/tables/small.blade.php
+++ resources/views/pages/setlists/admin/tables/small.blade.php
@@ -1,9 +1,11 @@
 <div class="row"> 
 	<div class="col-lg-8 col-12 mx-auto">
 		<div id="setlist"> 
-			@foreach($setlist->whereNull('finished_at') as $entry)
+			@forelse($setlist->whereNull('finished_at') as $entry)
 			@include('pages.setlists.admin.songRequests.small')
-			@endforeach
+			@empty
+			@include('components.empty')
+			@endforelse
 		</div>
 	</div>
 </div>
