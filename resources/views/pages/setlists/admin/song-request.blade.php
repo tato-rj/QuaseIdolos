8e26784d4f30d98a385e73113ca3545118795440
Arthur Villar <arthur@iMac.local>
2023-01-23 16:29:20 -0500

    Quick update

--- resources/views/pages/setlists/admin/song-request.blade.php
+++ resources/views/pages/setlists/admin/song-request.blade.php
@@ -18,7 +18,7 @@
 			</div>
 		</div>
 		<div class="col-lg-4 col-12">
-			<div class="h-100 ">
+			<div class="h-100 d-flex justify-content-center flex-column">
 				<div class="d-flex justify-content-end mb-2">
 					@if($entry->song->chords_url)
 					<a href="{{$entry->song->chords_url}}" title="Ver cifra" target="_blank" class="rounded-circle btn btn-secondary mr-2 d-center first-only" style="width: 38px; height: 38px;">@fa(['icon' => 'guitar', 'mr' => 0])</a>
@@ -32,7 +32,7 @@
 				@else
 				<button class="btn btn-stone mb-2 no-stroke">@fa(['icon' => 'hourglass-half'])@choice('FALTA|FALTAM', $entry->order) {{$entry->order}}</button>
 				@endif
-
+				
 				<button data-bs-toggle="modal" data-bs-target="#song-requests-cancel-{{$entry->id}}-modal" class="btn btn-outline-red no-stroke">CANCELAR</button>
 
 				@include('pages.song-requests.modals.finish')
