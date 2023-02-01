a742ffa9096b5a3cecf2ca082a8dee43a73d6ef0
Arthur Villar <arthur@iMac.local>
2023-02-01 15:50:43 -0500

    Quick update

--- resources/views/pages/setlists/admin/tables/large.blade.php
+++ resources/views/pages/setlists/admin/tables/large.blade.php
@@ -10,7 +10,7 @@
 @php($count = $setlist->whereNotNull('finished_at')->count())
 <div class="mt-5 pt-4" style="border-top: 8px dotted rgba(0,0,0,0.2)">
 	<h5 class="text-center mb-4">{{$count}} @choice('música|músicas', $count) já @choice('cantada|cantadas', $count)</h5>
-	@foreach($setlist->whereNotNull('finished_at')->sortByDesc('created_at') as $entry)
+	@foreach($setlist->whereNotNull('finished_at')->sortByDesc('finished_at') as $entry)
 	@include('pages.setlists.admin.confirmed')
 	@endforeach
 </div>
