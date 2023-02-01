8db11891a21e476d2edf94768569f2c1912a8261
Arthur Villar <arthur@iMac.local>
2023-02-01 14:20:46 -0500

    Quick update

--- resources/views/components/avatar/image.blade.php
+++ resources/views/components/avatar/image.blade.php
@@ -7,6 +7,7 @@ data-bs-target="#offcanvasUserMenu"
 @else
 class="nav-link d-center position-relative"
 @endisset
+style="width: {{$size ?? '39.2px'}}" 
 >
-  <img src="{{$user->avatar()}}" class="rounded-circle w-100 avatar-img {{$classes ?? null}}" style="width: {{$size ?? '39.2px'}}">
+  <img src="{{$user->avatar()}}" class="rounded-circle w-100 avatar-img {{$classes ?? null}}">
 </div>
