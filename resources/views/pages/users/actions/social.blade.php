4ae39a418e0c58c8642a16c9bd4301a3064e2b72
Arthur Villar <arthur@iMac.local>
2023-02-03 14:59:31 -0500

    Quick update

--- resources/views/pages/users/actions/social.blade.php
+++ resources/views/pages/users/actions/social.blade.php
@@ -1,4 +1,5 @@
 @foreach($user->socialAccounts as $socialAccount)
+<h1>ASD</h1>
 	<form method="POST" action="{{route('profile.unlink-social', $socialAccount)}}">
 		@csrf
 		<button type="submit" class="btn btn-{{$socialAccount->social_provider}} w-100 mb-2">@fa(['icon' => $socialAccount->social_provider, 'fa_type' => 'b'])Remover {{ucfirst($socialAccount->social_provider)}}</button>
