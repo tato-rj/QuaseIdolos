779933d80c95da79965e2ba9a5877772d49a854a
Arthur Villar <arthur@iMac.local>
2023-02-07 10:20:55 -0500

    Quick update

--- app/Http/Controllers/ViewsController.php
+++ app/Http/Controllers/ViewsController.php
@@ -9,7 +9,6 @@ class ViewsController extends Controller
 {
     public function home()
     {
-
         $artists = Artist::inRandomOrder()->visible()->has('songs')->orderby('name')->take(10)->get();
         $genres = Genre::inRandomOrder()->has('songs')->orderby('name')->take(10)->get();
         $topUsers = User::ranking()->take(5)->get();
---
+++ app/Models/TestSchedule.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class TestSchedule extends BaseModel
+{
+    //
+}
