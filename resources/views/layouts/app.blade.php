878bd3bae9d1a941d1cfce89c308b74f8b1cf92a
Arthur Villar <arthur@iMac.local>
2023-02-10 09:44:31 -0500

    Quick update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{$title ?? 'Welcome'}} | {{config('app.name')}}</title>
+        <title>{{$title ?? 'Bem vindo'}} | {{config('app.name')}} - Rio de Janeiro</title>
 
         @include('layouts.components.favicon')
         @include('layouts.components.seo')
