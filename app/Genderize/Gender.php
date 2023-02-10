7488fadfb20ee1e78673500efb43ce2c26b6a684
Arthur Villar <arthur@iMac.local>
2023-02-10 00:27:39 -0500

    Quick update

--- app/Genderize/Gender.php
+++ app/Genderize/Gender.php
@@ -8,13 +8,20 @@ class Gender
 
 	public function __construct($name)
 	{
-        $data = file_get_contents('http://api.genderize.io?' . http_build_query(['name' => $name]));
+		try {
+	        $data = file_get_contents('http://api.genderize.io?' . http_build_query(['name' => $name]));
 
-        $this->data = json_decode($data);
+	        $this->data = json_decode($data);	
+		} catch (\Exception $e) {
+			bugreport($e);
+		}
 	}
 
 	public function guess()
 	{
+		if (! $this->data)
+			return null;
+		
 		try {
 			if ($this->data->probability >= 0.5) {
 				return $this->data->gender;
