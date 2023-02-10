5f15d1bf0ed968e8303a943466ef3fc0e93b79f9
Arthur Villar <arthur@iMac.local>
2023-02-10 10:44:03 -0500

    Quick update

--- resources/views/pages/home/bands/schedule.blade.php
+++ resources/views/pages/home/bands/schedule.blade.php
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div id="schedule-box" class="col-lg-8 col-12 mx-auto animate__animated animate__fast animate__fadeInUp" style="display: none;">
 			<div class="border border-secondary rounded-pill border-5 px-3 py-4 text-center" style="background: #972139;">
-				<h4 class="m-0">@fa(['icon' => 'calendar-alt'])Shows semanais no <a href="https://bigbenpub.com.br/" target="_blank" class="link-secondary">Big Ben Pub</a>, em Botafogo RJ</h4>
+				<h4 class="m-0">@fa(['icon' => 'calendar-alt'])Shows semanais no <a href="https://bigbenpub.com.br/" target="_blank" class="link-secondary">Big Ben Pub</a>, em Botafogo/RJ</h4>
 			</div>
 		</div>
 	</div>
