b9046e686ac98b8b23748a23595ed5f945986a99
Arthur Villar <arthur@iMac.local>
2023-02-13 13:56:48 -0500

    Quick update

--- resources/views/components/floatingMenu.blade.php
+++ resources/views/components/floatingMenu.blade.php
@@ -1,17 +1,19 @@
 <div class="position-fixed d-flex align-items-center" style="bottom: 10px; right: 10px; z-index: 1;">
-    {{-- @local --}}
+    {{-- CHAT --}}
+    @admin
     @if(auth()->user()->liveGig)
     @include('components.chat.badge')
     @endif
-    {{-- @endlocal --}}
+    @endadmin
     
     @admin
     @include('pages.gigs.status')
     @endadmin
 </div>
 
-{{-- @local --}}
+{{-- CHAT --}}
+@admin
 @if(auth()->user()->liveGig)
 @include('components.chat.modal')
 @endif
-{{-- @endlocal --}}
+@endadmin
