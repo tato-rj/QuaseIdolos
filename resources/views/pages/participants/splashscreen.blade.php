3b75f13899973580ca047f7d60417dd9b1421a00
Arthur Villar <arthur@iMac.local>
2023-02-15 13:02:20 -0500

    Quick update

--- resources/views/pages/participants/splashscreen.blade.php
+++ resources/views/pages/participants/splashscreen.blade.php
@@ -1,11 +1,11 @@
 <div class="position-fixed bg-primary top-0 left-0 w-100 h-100vh" style="z-index: 10000000;" id="participant-video-confirmed-container">
 	<div class="h-100 w-100 d-center">
 		<video style="width: 100%; transition: 1s;" id="participant-video-confirmed" muted="muted" data-target="#participant-video-confirmed-container">
-			@if($agent->isMobile())
+			{{-- @if($agent->isMobile()) --}}
 			  <source src="{{asset('videos/invitation_splash_vertical.mp4')}}" type="video/mp4" />
-			@else
-			  <source src="{{asset('videos/invitation_splash_horizontal.mp4')}}" type="video/mp4" />
-			@endif
+			{{-- @else --}}
+			  {{-- <source src="{{asset('videos/invitation_splash_horizontal.mp4')}}" type="video/mp4" /> --}}
+			{{-- @endif --}}
 		</video>
 	</div>
 </div>
