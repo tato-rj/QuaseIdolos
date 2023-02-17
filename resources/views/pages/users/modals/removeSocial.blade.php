98f93b6b848a77d0f42f456abfb068922adb3a5d
Arthur Villar <arthur@iMac.local>
2023-02-17 12:57:39 -0500

    Quick update

--- resources/views/pages/users/modals/removeSocial.blade.php
+++ resources/views/pages/users/modals/removeSocial.blade.php
@@ -1,7 +1,7 @@
 @modal(['title' => 'Tem certeza?','id' => 'remove-social-'.$socialAccount->id.'-modal'])
 <form method="POST" action="{{route('profile.unlink-social', $socialAccount)}}">
 	@csrf
-		<p class="">Quer desautorizar o login com <strong>{{ucfirst($socialAccount->social_provider)}}</strong>?</p>
+		<p class="">Quer desautorizar o login com <u>{{ucfirst($socialAccount->social_provider)}}</u>?</p>
 
 	@submit(['label' => 'Sim, pode continuar', 'theme' => 'secondary'])
 </form>
