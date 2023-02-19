c6e1929a3f521b45c19267e988da552fd3e64a75
Arthur Villar <arthur@iMac.local>
2023-02-19 12:32:29 -0500

    Quick update

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,7 +11,7 @@ class UsersController extends Controller
 {
     public function index()
     {
-        return User::search('bruna')->orderBy('name')->with('allMessages')->get();
+        return User::search('bruna')->orderBy('name')->with('allMessages')->first();
         return User::withCount(['participations', 'songRequests', 'favorites', 'allMessages'])->sortable()->get();
 
         $users = User::guests()->withCount(['participations', 'songRequests', 'favorites', 'allMessages'])->sortable()->paginate(12);
