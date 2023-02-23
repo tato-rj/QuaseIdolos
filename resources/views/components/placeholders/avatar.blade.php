aef7fcf1234bdcf12ab173b543458a4f58777123
Arthur Villar <arthur@iMac.local>
2023-02-23 14:02:16 -0500

    Quick update

--- resources/views/components/placeholders/avatar.blade.php
+++ resources/views/components/placeholders/avatar.blade.php
@@ -1,1 +1,4 @@
-<div class="bg-transparent rounded-circle m-2 placeholder-animate" style="width: 60px; height: 60px;"></div>
+<div class="m-3">
+	<div class="bg-transparent rounded-circle mb-2 placeholder-animate" style="width: 60px; height: 60px;"></div>
+	<div class="placeholder-animate rounded bg-transparent" style="width: 60px; height: 12px"></div>
+</div>
