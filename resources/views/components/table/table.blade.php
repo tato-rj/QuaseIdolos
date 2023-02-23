2b4379d1aaef28fe13087bea9fd4a4f1b55d3635
Arthur Villar <arthur@iMac.local>
2023-02-23 12:43:55 -0500

    Quick update

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,30 +1,32 @@
-<div class="results-container table-responsive">
-  <table class="table w-auto table-borderless table-striped text-nowrap table-hover">
-    @if($header)
-    <thead>
-      <tr>
-        @foreach($columns as $field => $label)
-        <th scope="col" class="text-white px-3">{{$label}}</th>
+<div class="results-container">
+  <div class="table-responsive">
+    <table class="table w-auto table-borderless table-striped text-nowrap table-hover">
+      @if($header)
+      <thead>
+        <tr>
+          @foreach($columns as $field => $label)
+          <th scope="col" class="text-white px-3">{{$label}}</th>
+          @endforeach
+        </tr>
+      </thead>
+      @endif
+      <tbody>
+        @foreach($rows as $row)
+        <tr>
+          @foreach($columns as $field => $label)
+            <td class="{{$field == 'actions' ? 'text-right' : null}} align-middle px-3 py-3">
+              @if($field == 'actions')
+              @include($view)
+              @else
+              <h6 class="m-0 text-white">@include($view)</h6>
+              @endif
+            </td>
+          @endforeach
+        </tr>
         @endforeach
-      </tr>
-    </thead>
-    @endif
-    <tbody>
-      @foreach($rows as $row)
-      <tr>
-        @foreach($columns as $field => $label)
-          <td class="{{$field == 'actions' ? 'text-right' : null}} align-middle px-3 py-3">
-            @if($field == 'actions')
-            @include($view)
-            @else
-            <h6 class="m-0 text-white">@include($view)</h6>
-            @endif
-          </td>
-        @endforeach
-      </tr>
-      @endforeach
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
 
 
   @if($hasPagination)
