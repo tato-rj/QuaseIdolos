5dca9cfae1e98897a082406e6bc5645e0d82f93b
Arthur Villar <arthur@iMac.local>
2023-02-23 19:03:48 -0500

    Quick update

--- resources/views/components/song/admin/icons.blade.php
+++ resources/views/components/song/admin/icons.blade.php
@@ -1,6 +1,6 @@
 @php($completed_count = $song->completed_count)
 @php($favorites_count = $song->favorites_count)
 
-<span class="small mr-1 {{$completed_count == 0 ? 'opacity-4' : 'text-teal'}}">@fa(['icon' => 'microphone', 'mr' => 0]) {{$completed_count}}</span>
+<span class="small mr-1 {{$completed_count == 0 ? 'opacity-4' : 'text-secondary'}}">@fa(['icon' => 'microphone', 'mr' => 0]) {{$completed_count}}</span>
 
-<span class="small {{$favorites_count == 0 ? 'opacity-4' : 'text-blue'}}">@fa(['icon' => 'heart', 'mr' => 0]) {{$favorites_count}}</span>
+<span class="small {{$favorites_count == 0 ? 'opacity-4' : 'text-secondary'}}">@fa(['icon' => 'heart', 'mr' => 0]) {{$favorites_count}}</span>
