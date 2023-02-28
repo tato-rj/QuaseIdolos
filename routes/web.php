609a7df9f65b3ca9bd890bbdc1414e89e5d7d0ca
Arthur Villar <arthur@iMac.local>
2023-02-27 21:39:07 -0500

    Quick update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 
-Route::domain('{gig}.quaseidolos.test')->group(function () {
+Route::domain('{gig}.quaseidolos.com.br')->group(function () {
     Route::get('', function ($gig) {
         return $gig;
     });
