0f84d9e73404df8dadeac4d942e332e1d22315c9
Arthur Villar <arthur@iMac.local>
2023-03-02 10:41:18 -0500

    Quick update

--- resources/views/pages/setlists/admin/index.blade.php
+++ resources/views/pages/setlists/admin/index.blade.php
@@ -3,9 +3,9 @@
 @push('header')
 <style type="text/css">
 @keyframes metronome {
-  0%   {width: 50%; height: 50%}
+  0%   {width: 50%; height: 50%; opacity: 1}
   90% {opacity: 0}
-  100% {width: 100%; height: 100%; opacity: 0}
+  100% {width: 100%; height: 100%; opacity: 0;}
 }
 
 .pulse {
--- resources/views/pages/songs/metronome/ring.blade.php
+++ resources/views/pages/songs/metronome/ring.blade.php
@@ -1,2 +1,2 @@
 <div class="ring position-absolute-center rounded-circle border border-secondary pulse" 
-	style="animation-duration: {{60/$song->bpm}}s;"></div>
+	style="animation-duration: {{60/$song->bpm}}s; opacity: 0"></div>
