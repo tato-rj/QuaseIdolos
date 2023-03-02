378613e7042ba546b04aff0563364fb1e61d13e2
Arthur Villar <arthur@iMac.local>
2023-03-02 10:47:46 -0500

    Quick update

--- resources/views/pages/setlists/admin/index.blade.php
+++ resources/views/pages/setlists/admin/index.blade.php
@@ -9,12 +9,6 @@
   100% {width: 100%; height: 100%; opacity: 0;}
 }
 
-@keyframes metronome {
-  0%   {width: 50%; height: 50%; opacity: 1}
-  90% {opacity: 0}
-  100% {width: 100%; height: 100%; opacity: 0;}
-}
-
 .pulse {
 	opacity: 0;
 	width: 50%;
