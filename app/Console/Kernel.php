2be5c117a98225d42e56ef8e593683d5b91fcc89
Arthur Villar <arthur@iMac.local>
2023-03-15 12:03:37 -0400

    Quick update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('gigs:close')->everyMinute();
+        $schedule->command('gigs:close')->everyHour();
     }
 
     /**
