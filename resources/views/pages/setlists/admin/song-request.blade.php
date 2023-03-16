2b179230148acb35834b958035e9ce946b72cb09
Arthur Villar <arthur@iMac.local>
2023-03-16 10:00:31 -0400

    Quick update

--- resources/views/pages/setlists/admin/song-request.blade.php
+++ resources/views/pages/setlists/admin/song-request.blade.php
@@ -15,7 +15,7 @@
 			</div>
 		</div>
 		<div class="col-lg-4 col-12">
-			{{-- <div class="h-100 d-flex justify-content-center flex-column"> --}}
+			<div class="h-100 d-flex justify-content-center flex-column">
 				<div class="d-flex justify-content-end mb-2">
 					@if($entry->song->chords_url)
 					<a href="{{$entry->song->chords_url}}" title="Ver cifra" target="_blank" class="rounded-circle btn btn-secondary mr-2 d-center" style="width: 38px; height: 38px;">@fa(['icon' => 'guitar', 'mr' => 0])</a>
@@ -36,7 +36,7 @@
 				@include('pages.song-requests.modals.finish')
 				@include('pages.song-requests.modals.change', ['songRequest' => $entry])
 				@include('pages.song-requests.modals.cancel')
-			{{-- </div> --}}
+			</div>
 		</div>
 	</div>
 </div>
