b2b94042af4025d25542f4155a01f2d69cff0b6b
Arthur Villar <arthur@iMac.local>
2023-03-16 09:57:15 -0400

    Quick update

--- resources/views/pages/setlists/admin/tables/metronome.blade.php
+++ resources/views/pages/setlists/admin/tables/metronome.blade.php
@@ -1,6 +1,6 @@
 <div class="row mb-4"> 
 	<div class="col-lg-8 col-12 mx-auto">
-		<div id="setlist" class="border border-secondary rounded" style="max-height: 300px; overflow-y: scroll;"> 
+		<div class="border border-secondary rounded" style="max-height: 300px; overflow-y: scroll;"> 
 			@forelse($setlist->whereNull('finished_at') as $entry)
 			@include('pages.setlists.admin.songRequests.metronome')
 			@empty
