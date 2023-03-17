9f61126ede1896462f559b17df75f611f0b9decc
Arthur Villar <arthur@iMac.local>
2023-03-17 14:07:04 -0400

    Quick update

--- app/Http/Controllers/RecommendationsController.php
+++ app/Http/Controllers/RecommendationsController.php
@@ -23,7 +23,7 @@ class RecommendationsController extends Controller
 
         // $seeder = SpotifySeed::addArtists(['4NHQUGzhtTLFvgF5SZesLK'])->addGenres(['classical', 'country'])->addTracks(['0c6xIDDpzE81m2q797ordA']);
 
-            $seeder = SpotifySeed::addArtists($songs->pluck('artist.spotify_id'))->addGenres($songs->pluck('genre.name'))->addTracks($songs->pluck('spotify_id'));
+            $seeder = SpotifySeed::addArtists($songs->pluck('artist.spotify_id'))->addGenres([$songs->first()->genre->name])->addTracks($songs->pluck('spotify_id'));
 
             return SpotifyApi::recommendations($seeder)->limit(10)->get();   
         // } catch (\Exception $e) {
