58ed713347237a6173645bf6d6c3706ac71052af
Arthur Villar <arthur@iMac.local>
2023-03-17 10:25:13 -0400

    Quick update

--- resources/views/pages/setlists/admin/tables/metronome.blade.php
+++ resources/views/pages/setlists/admin/tables/metronome.blade.php
@@ -1,5 +1,6 @@
 <div class="row mb-4" id="setlist"> 
 	<div class="col-lg-8 col-12 mx-auto">
+		<p class="fw-bold opacity-6 text-center">Escolha a música para ligar o metrônomo</p>
 		<div class="border border-secondary rounded" style="max-height: 300px; overflow-y: scroll;"> 
 			@forelse($setlist->whereNull('finished_at') as $entry)
 			@include('pages.setlists.admin.songRequests.metronome')
