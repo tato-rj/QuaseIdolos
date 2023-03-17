749c61e0f0150781774bf173005b6b9e1417a6de
Arthur Villar <arthur@iMac.local>
2023-03-17 14:18:06 -0400

    Quick update

--- app/Http/Controllers/RecommendationsController.php
+++ app/Http/Controllers/RecommendationsController.php
@@ -14,21 +14,15 @@ class RecommendationsController extends Controller
     {
         // return SpotifyApi::track('2WjLc16JdLH2V6FMk8VFsZ')->get();
         // return SpotifyApi::searchTracks('João Penca & Seus Miquinhos Amestrados - popstar')->limit(5)->get();
-        $songs = Song::inRandomOrder()->take(3)->get();
-        // return $songs->pluck('spotify_id');
-        // try {
-            
-// return $songs->pluck('spotify_id');
-        // $seeder = SpotifySeed::addArtists([$songs->first()->artist->spotify_id])->addGenres([$songs->first()->genre->name])->addTracks([$songs->first()->spotify_id]);      
+        $songs = Song::inRandomOrder()->take(3)->get();   
 
-        $seeder = SpotifySeed::addArtists($songs->first()->artist->spotify_id)->addGenres($songs->pluck('genre.name'))->addTracks($songs->pluck('spotify_id'));
+        $seeder = SpotifySeed::addArtists($songs->first()->artist->spotify_id)
+                             ->addGenres($songs->pluck('genre.name'))
+                             ->addTracks($songs->pluck('spotify_id'));
 
-            // $seeder = SpotifySeed::addArtists($songs->pluck('artist.spotify_id'))->addGenres($songs->pluck('genre.name'))->addTracks($songs->pluck('spotify_id'));
+        $results = SpotifyApi::recommendations($seeder)->limit(10)->get();
 
-            return SpotifyApi::recommendations($seeder)->limit(10)->get();   
-        // } catch (\Exception $e) {
-            // bugreport($e);
-        // }
+        return randval($results['tracks']);
     }
 }
 
