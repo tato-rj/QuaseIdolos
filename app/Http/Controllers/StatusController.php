e35b6cadb7dde6d3c0d619784cb10514986bccad
Arthur Villar <arthur@iMac.local>
2023-03-17 11:56:17 -0400

    Quick update

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -10,32 +10,32 @@ class StatusController extends Controller
 {
     public function index()
     {
-        $users = (new AppStatus(User::class))->hasMany([
-            Suggestion::class,
-            Favorite::class, 
-            SongRequest::class,
-            Participant::class, 
-            SocialAccount::class])->check();
+        // $users = (new AppStatus(User::class))->hasMany([
+        //     Suggestion::class,
+        //     Favorite::class, 
+        //     SongRequest::class,
+        //     Participant::class, 
+        //     SocialAccount::class])->check();
 
-        $songRequests = (new AppStatus(SongRequest::class))->belongsTo([
-            Gig::class, 
-            User::class,
-            Song::class])->check();
+        // $songRequests = (new AppStatus(SongRequest::class))->belongsTo([
+        //     Gig::class, 
+        //     User::class,
+        //     Song::class])->check();
 
-        $songs = (new AppStatus(Song::class))->hasMany([
-            Favorite::class, 
-            SongRequest::class])->belongsTo([Artist::class])->check();
+        // $songs = (new AppStatus(Song::class))->hasMany([
+        //     Favorite::class, 
+        //     SongRequest::class])->belongsTo([Artist::class])->check();
 
-        $artists = (new AppStatus(Artist::class))->hasMany([
-            Song::class])->check();
+        // $artists = (new AppStatus(Artist::class))->hasMany([
+        //     Song::class])->check();
 
-        $report = collect([
-            User::class => $users,
-            SongRequest::class => $songRequests,
-            Song::class => $songs,
-            Artist::class => $artists
-        ]);
+        // $report = collect([
+        //     User::class => $users,
+        //     SongRequest::class => $songRequests,
+        //     Song::class => $songs,
+        //     Artist::class => $artists
+        // ]);
 
-        return view('status.index', compact(['report']));
+        // return view('status.index', compact(['report']));
     }
 }
