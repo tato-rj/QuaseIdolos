8bf33a5d968b02f979da784bd63ffd3dcec4ee2a
Arthur Villar <arthur@iMac.local>
2023-03-17 13:45:42 -0400

    Quick update

--- app/Http/Controllers/RecommendationsController.php
+++ app/Http/Controllers/RecommendationsController.php
@@ -11,10 +11,13 @@ class RecommendationsController extends Controller
 {
     public function get()
     {
-        return SpotifyApi::searchTracks('balao magico - superfantastico')->limit(5)->get();
+        // return SpotifyApi::track('2WjLc16JdLH2V6FMk8VFsZ')->get();
+        // return SpotifyApi::searchTracks('João Penca & Seus Miquinhos Amestrados - popstar')->limit(5)->get();
 
         $seeder = SpotifySeed::addArtists(['4NHQUGzhtTLFvgF5SZesLK'])->addGenres(['classical', 'country'])->addTracks(['0c6xIDDpzE81m2q797ordA']);
 
         return SpotifyApi::recommendations($seeder)->limit(10)->get();
     }
 }
+
+https://open.spotify.com/track/2WjLc16JdLH2V6FMk8VFsZ?si=5f57556745b746be
