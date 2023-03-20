590f24db5ec0177b1a56db87f3ec349184059fca
Arthur Villar <arthur@iMac.local>
2023-03-20 16:38:01 -0400

    Quick update

--- resources/views/pages/cardapio/components/recommendation/song.blade.php
+++ resources/views/pages/cardapio/components/recommendation/song.blade.php
@@ -2,7 +2,7 @@
 	<div class="rounded bg-transparent p-3 h-100 t-2 border-secondary border-3 choice-song" data-id="{{$song->id}}">
 		<div class="mb-1">
 			<img src="{{$song->artist->coverImage()}}" class="rounded-circle mb-1" style="width: 80%">
-			<p class="m-0 opacity-8">{{$song->artist->name}}</p>
+			<p class="m-0 opacity-8 text-truncate">{{$song->artist->name}}</p>
 		</div>
 		<h6 class="text-secondary text-truncate mb-3">{{$song->name}}</h6>
 		<div>
