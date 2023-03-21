3c590e22f112f7e923aba1cd4f1b9a31c095a990
Arthur Villar <arthur@iMac.local>
2023-03-21 11:22:41 -0400

    Quick update

--- resources/views/components/placeholders/recommendation.blade.php
+++ resources/views/components/placeholders/recommendation.blade.php
@@ -2,11 +2,11 @@
 <div class="text-center col-lg-3 col-md-4 col-6 mb-3">
 	<div class="rounded bg-transparent placeholder-animate p-3 h-100 border-secondary">
 		<div class="rounded-circle mb-4 bg-transparent mx-auto" style="width: 100px; height: 100px"></div>
-		@include('components.placeholders.block', ['height' => '30px', 'width' => '100%', 'classes' => 'mb-1'])
-		@include('components.placeholders.block', ['height' => '30px', 'width' => '100%', 'classes' => 'mb-4'])
+		@include('components.placeholders.block', ['height' => '20px', 'width' => '100%', 'classes' => 'mb-1'])
+		@include('components.placeholders.block', ['height' => '20px', 'width' => '100%', 'classes' => 'mb-4'])
 		<div class="mt-4">
-			@include('components.placeholders.block', ['height' => '30px', 'width' => '100%', 'classes' => 'mb-1'])
-			@include('components.placeholders.block', ['height' => '30px', 'width' => '100%'])
+			@include('components.placeholders.block', ['height' => '20px', 'width' => '100%', 'classes' => 'mb-1'])
+			@include('components.placeholders.block', ['height' => '20px', 'width' => '100%'])
 		</div>
 	</div>
 </div>
