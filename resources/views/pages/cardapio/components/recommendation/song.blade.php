650a98963fc6c71618d6aeae8a8e615d0fe2717a
Arthur Villar <arthur@iMac.local>
2023-03-21 11:04:33 -0400

    Quick update

--- resources/views/pages/cardapio/components/recommendation/song.blade.php
+++ resources/views/pages/cardapio/components/recommendation/song.blade.php
@@ -1,10 +1,8 @@
 <div class="text-center col-lg-3 col-md-4 col-6 mb-3">
 	<div class="rounded bg-transparent p-3 h-100 t-2 border-secondary border-3 choice-song" data-id="{{$song->id}}">
-		<div class="mb-1">
-			<img src="{{$song->artist->coverImage()}}" class="rounded-circle mb-1" style="width: 80%">
-			<p class="m-0 opacity-8 text-truncate">{{$song->artist->name}}</p>
-		</div>
-		<h6 class="text-secondary text-truncate mb-3">{{$song->name}}</h6>
+		<img src="{{$song->artist->coverImage()}}" class="rounded-circle mb-1" style="width: 80%">
+		<p class="m-0 opacity-8 text-truncate">{{$song->artist->name}}</p>
+		<h6 class="text-secondary text-truncate mb-2">{{$song->name}}</h6>
 		<div>
 			<button class="btn btn-outline-secondary btn-sm mb-2 w-100 preview-song" data-src="{{$song->preview_url}}" style="white-space: nowrap;">@fa(['icon' => 'play'])Ouvir</button>
 			<button class="btn btn-secondary btn-sm w-100 choose-song">Escolher</button>
