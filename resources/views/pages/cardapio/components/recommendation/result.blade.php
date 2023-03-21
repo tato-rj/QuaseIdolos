67e3560e76597c2dc4168ca987b2dd8fc71e7c21
Arthur Villar <arthur@iMac.local>
2023-03-21 11:58:19 -0400

    Quick update

--- resources/views/pages/cardapio/components/recommendation/result.blade.php
+++ resources/views/pages/cardapio/components/recommendation/result.blade.php
@@ -2,7 +2,7 @@
 	<div class="mb-4" data-id="{{$song->id}}">
 		<img src="{{$song->artist->coverImage()}}" class="rounded-circle mb-2" style="width: 140px">
 		<h5 class="m-0">{{$song->artist->name}}</h5>
-		<h3 class="text-secondary">{{$song->name}}</h3>
+		<h4 class="text-secondary">{{$song->name}}</h4>
 		<button class="btn btn-outline-secondary btn-sm preview-song" data-src="{{$song->preview_url}}" style="white-space: nowrap;">@fa(['icon' => 'play'])Ouvir</button>
 	</div>
 
