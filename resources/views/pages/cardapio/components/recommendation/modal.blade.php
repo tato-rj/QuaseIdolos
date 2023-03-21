7131d47606fa765ffcb4b0a184ed593f450026f3
Arthur Villar <arthur@iMac.local>
2023-03-21 11:02:40 -0400

    Quick update

--- resources/views/pages/cardapio/components/recommendation/modal.blade.php
+++ resources/views/pages/cardapio/components/recommendation/modal.blade.php
@@ -7,7 +7,7 @@
 	'data' => ['url' => '']
 ])
 <div id="recommendation-container">
-	<p class="text-center mb-4"><span class="opacity-6">Escolha abaixo as</span> <span class="h5">5</span> <span class="opacity-6">músicas que você mais gosta dessa lista:</span></p>
+	<p class="text-center mb-4"><span class="opacity-6">Escolha abaixo as</span> <span class="h5 fw-bold">5</span> <span class="opacity-6">músicas que você mais gosta dessa lista:</span></p>
 	
 	<div class="row justify-content-center mb-3">
 		@foreach($choices as $song)
