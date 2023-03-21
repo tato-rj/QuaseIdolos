c24f0871b15f8c833266f6e00970b34b8b6fc015
Arthur Villar <arthur@iMac.local>
2023-03-21 11:00:54 -0400

    Quick update

--- resources/views/pages/cardapio/components/recommendation/result.blade.php
+++ resources/views/pages/cardapio/components/recommendation/result.blade.php
@@ -1,8 +1,8 @@
 <div class="text-center">
 	<div class="p-3 h-100" data-id="{{$song->id}}">
-		<img src="{{$song->artist->coverImage()}}" class="rounded-circle mb-2" style="width: 120px">
-		<h6 class="">{{$song->artist->name}}</h6>
-		<h4 class="text-secondary">{{$song->name}}</h4>
+		<img src="{{$song->artist->coverImage()}}" class="rounded-circle mb-2" style="width: 140px">
+		<h5 class="">{{$song->artist->name}}</h5>
+		<h3 class="text-secondary">{{$song->name}}</h3>
 	</div>
 	<button data-bs-toggle="modal" data-bs-target="#song-{{$song->id}}-modal" class="btn btn-secondary">@fa(['icon' => 'guitar'])Quero essa!</button>
 
