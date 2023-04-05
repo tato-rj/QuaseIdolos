bde6e9d2331697772abde3b88ccff70f658a8e2d
Arthur Villar <arthur@iMac.local>
2023-04-05 10:11:23 -0400

    Quick update

--- resources/views/pages/gigs/banner.blade.php
+++ resources/views/pages/gigs/banner.blade.php
@@ -13,7 +13,7 @@
 		@endif
 	</div>
 </div>
-<div class="bg-secondary text-center fw-bold px-1 text-primary border-top boder border-primary">
-	Aguarde um pouquinho... as inscrições já vão voltar!
+<div class="bg-white text-center fw-bold px-1 text-primary border-top boder border-primary">
+	@fa(['icon' => 'exclamation-triangle'])Aguarde um pouquinho... as inscrições já vão voltar!
 </div>
 @endif
