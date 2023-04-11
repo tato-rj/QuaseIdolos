adc699e0c71cb8412c841832f9fecc873df92241
Arthur Villar <arthur@iMac.local>
2023-04-11 18:15:47 -0400

    Quick update

--- resources/views/pages/cardapio/results/row.blade.php
+++ resources/views/pages/cardapio/results/row.blade.php
@@ -9,11 +9,16 @@
     
     @include('pages.cardapio.results.row.name')
   </div>
+
+
+    <button data-bs-toggle="modal" data-bs-target="#song-{{$song->id}}-modal" class="btn btn-secondary btn-sm text-truncate w-100 mt-2">Quero cantar essa música</button>
+      @include('pages.cardapio.components.song.modal')
+
   @endslot
 
-  @slot('actions')
-  <button data-bs-toggle="modal" data-bs-target="#song-{{$song->id}}-modal" class="btn btn-secondary text-truncate">@fa(['icon' => 'microphone', 'mr' => 0])</button>
+  {{-- @slot('actions') --}}
+  {{-- <button data-bs-toggle="modal" data-bs-target="#song-{{$song->id}}-modal" class="btn btn-secondary text-truncate">@fa(['icon' => 'microphone', 'mr' => 0])</button> --}}
 
-  @include('pages.cardapio.components.song.modal')
-  @endslot
+  {{-- @include('pages.cardapio.components.song.modal') --}}
+  {{-- @endslot --}}
 @endresponsiveRow
