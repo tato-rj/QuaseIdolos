16a1d79434564e7d492300ab3546cb9e1a23f9f3
Arthur Villar <arthur@iMac.local>
2023-10-13 14:49:41 -0400

    Quick update

--- resources/views/pages/cardapio/components/song/info.blade.php
+++ resources/views/pages/cardapio/components/song/info.blade.php
@@ -3,12 +3,12 @@
 		@include('pages.cardapio.components.song.actions')
 	</div>
 	<div class="mb-">
-		<div class="py-2 mb-4 text-center" style="border-top: 10px dotted rgba(0,0,0,.2); border-bottom: 10px dotted rgba(0,0,0,.2);">
+{{-- 		<div class="py-2 mb-4 text-center" style="border-top: 10px dotted rgba(0,0,0,.2); border-bottom: 10px dotted rgba(0,0,0,.2);">
 			<h5>@lang('views/cardapio.song.avg-score')</h5>
 			<div class="d-center rating">
 				@include('pages.ratings.stars', ['rating' => round($song->ratings->avg('score'))])
 			</div>
-		</div>
+		</div> --}}
 		<div>
 			<div class="d-apart mb-2">
 				<h5 class="text-secondary no-stroke">@fa(['icon' => 'guitar'])@lang('views/cardapio.song.genre')</h5>
