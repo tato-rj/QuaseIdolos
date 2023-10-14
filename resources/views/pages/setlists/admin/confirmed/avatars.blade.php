0ca5b8b359cb2566e8aeee22e84d0e21108fe268
Arthur Villar <arthur@iMac.local>
2023-10-13 21:45:17 -0400

    Quick update

--- resources/views/pages/setlists/admin/confirmed/avatars.blade.php
+++ resources/views/pages/setlists/admin/confirmed/avatars.blade.php
@@ -2,8 +2,8 @@
 
 <div style="margin-left: -18px" class="mb-1">
 	@if($user->hasAvatar())
-	@include('components.avatar.image', ['size' => '56px', 'classes' => 'border border-1'])
+	@include('components.avatar.image', ['size' => '46px', 'classes' => 'border border-1'])
 	@else
-	@include('components.avatar.initial', ['size' => '56px', 'classes' => 'border border-1'])
+	@include('components.avatar.initial', ['size' => '46px', 'classes' => 'border border-1'])
 	@endif
 </div>
