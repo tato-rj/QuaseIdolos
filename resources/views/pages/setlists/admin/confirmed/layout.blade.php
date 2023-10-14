4e6879c8e22243ee61b06a440963d86889dee050
Arthur Villar <arthur@iMac.local>
2023-10-13 21:42:29 -0400

    Quick update

--- resources/views/pages/setlists/admin/confirmed/layout.blade.php
+++ resources/views/pages/setlists/admin/confirmed/layout.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-	<div class="col-lg-8 col-md-12 col-12 mx-auto">
+	<div class="col-lg-8 col-md-10 col-10 mx-auto">
 	<div class="d-flex flex-wrap">
 		@foreach($setlist->whereNotNull('finished_at')->sortBy('finished_at') as $entry)
 		@include('pages.setlists.admin.confirmed.avatars')
